ac1502e64c53081955d3b33e319642286719fb88
EronVirson <eronvirson@live.com>
2018-03-13 15:36:27 -0400

    3/13 More Tweaks

--- Apollyon's Curse/Assets/PlayerController.cs
+++ Apollyon's Curse/Assets/PlayerController.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	float fSpeed = 400.0f;
-	public Vector3 jump;
 	[Range(1,10)]
-	public float jumpForce = 5.0f;
+	public float jumpForce = 9.0f;
 
 	public float fallMultiplier = 2.5f;
 	public float lowJumpMultiplier = 2f;
@@ -17,10 +16,12 @@ public class PlayerController : MonoBehaviour {
 	Rigidbody rb;
     public Vector3 currentVelocity;
 
+    public float tapSpeed = 0.5f;
+    private float lastTapTime = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
-		jump = new Vector3 (0.0f, 2.0f, 0.0f);
 	}
 
 	void OnCollisionEnter(){
@@ -29,19 +30,38 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Capture current velocity
         currentVelocity = rb.velocity;
         //Left Right movement
         currentVelocity.x = 0.0f;
         currentVelocity += Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * fSpeed;
 
-        /*
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * fSpeed;
-        transform.Rotate(0, 0, 0);
-        transform.Translate(x, 0, 0);
-        */
-		//Basic jump
-		if (Input.GetButtonDown ("Jump") && isGrounded ) {
-			currentVelocity = jump * jumpForce;
+        //Double Tap Dash
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if((Time.time - lastTapTime) < tapSpeed)
+            {
+                //Tap Success
+                Debug.Log("Double A");
+                //currentVelocity += Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * fSpeed * 100;
+            }
+            lastTapTime = Time.time;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if ((Time.time - lastTapTime) < tapSpeed)
+            {
+                //Tap Success
+                Debug.Log("Double D");
+                //currentVelocity += Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * fSpeed * 100;
+            }
+            lastTapTime = Time.time;
+        }
+        
+
+        //Basic jump
+        if (Input.GetButtonDown ("Jump") && isGrounded ) {
+			currentVelocity = Vector3.up * jumpForce;
 			isGrounded = false;
 		}
 		//Better Jump
@@ -51,6 +71,7 @@ public class PlayerController : MonoBehaviour {
 		else if(currentVelocity.y > 0 && !Input.GetButton("Jump")) {
 			currentVelocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
 		}
+        //Send velocity update
         rb.velocity = currentVelocity;
     }
 }
