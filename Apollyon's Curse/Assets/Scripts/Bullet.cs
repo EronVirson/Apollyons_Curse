38f75b366acf26b779795159b8e49aad08c03fda
EronVirson <eronvirson@live.com>
2018-04-03 13:40:20 -0400

    Turret Implemented
    It shoots stuff

---
+++ Apollyon's Curse/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed = 10;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+        rb.velocity = transform.forward * speed;
+	}
+}
---
+++ Apollyon's Curse/Assets/Scripts/TurretAiming.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAiming : MonoBehaviour {
+    public GameObject player;
+    public float timer = 5.0f;
+    public GameObject muzzle;
+    public float triggerDistance = 0.5f;
+    public float reloadSpeed = 5;
+    public GameObject bullet;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Player nearby and below?
+        if ((Vector3.Distance(transform.position, player.transform.position) <= triggerDistance) && transform.position.y >= player.transform.position.y)
+        {
+            //Shoot em!
+            this.transform.LookAt(player.transform);
+            if (timer > 0.0f)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                timer = 2.0f;
+                GameObject bang = Instantiate(bullet);
+                bang.transform.position = muzzle.transform.position;
+                bang.transform.forward = this.transform.forward;
+            }
+        }
+    }
+}
