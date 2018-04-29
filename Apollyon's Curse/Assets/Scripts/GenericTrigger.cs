792d5d384865bebaf432430cd1f17d2844e185fb
EronVirson <eronvirson@live.com>
2018-04-29 13:16:38 -0400

    All your base
    The start of something new

---
+++ Apollyon's Curse/Assets/Scripts/GenericTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTrigger : MonoBehaviour {
+
+    public bool isTouchingPlayer = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isTouchingPlayer = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            isTouchingPlayer = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
